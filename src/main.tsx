8243f975e7791a29c576f36148b018c1dbc3edfc
Ras Leon <rastavo@Rass-MacBook-Pro.local>
2025-04-09 18:10:41 -0400

    🔥 Fix blank page on Netlify

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,12 @@ import App from "./App.tsx";
 import "./index.css";
 import { WalletConnectionProvider } from "./contexts/WalletConnectionProvider";
 
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
 // Simple error boundary component
 class ErrorBoundary extends React.Component<{ children: React.ReactNode }, { hasError: boolean }> {
   constructor(props: { children: React.ReactNode }) {
